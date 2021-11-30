705156f582a8d1ec27bf5f51c67f0707966325e8
Jesse Friedman <jesse@jesse.ws>
2021-11-29 21:04:41 -0500

    Parse authentication data from environment variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const TweetStream = require('./lib/twitter');
 const WS = require('./lib/ws');
 
+const AUTH_DATA = process.env['TWEETPOLITE_AUTH'] ? JSON.parse(process.env['TWEETPOLITE_AUTH']) : require('./twitterauth.json');
+
 var ws = new WS({
   port: process.env.PORT || 80,
   logConnections: true
@@ -17,6 +19,6 @@ var onTweet = function(tweet) {
 }
 
 var stream = new TweetStream({
-  auth: process.env['TWEETPOLITE_AUTH'] || require('./twitterauth.json'),
+  auth: AUTH_DATA,
   ontweet: onTweet
 });
